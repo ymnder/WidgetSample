e651d26a756aa3d060c8de6f2898651a6694457e
ymnder <yamazaki.waseda@gmail.com>
2018-02-04 11:23:05 +0900

    add init widget files

---
+++ app/src/main/java/com/example/ymnd/widgetsample/NewAppWidget.kt
@@ -0,0 +1,51 @@
+package com.example.ymnd.widgetsample
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [NewAppWidgetConfigureActivity]
+ */
+class NewAppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            NewAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
+                                     appWidgetId: Int) {
+
+            val widgetText = NewAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
